959cfded7a022c3a9916f95308d55602eecd5e8f
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-04-15 14:13:38 -0500

    [perf♻] create a CreateAreaSpace to apply solid principles in PlayGame

--- BattleShip.java
+++ BattleShip.java
@@ -1,9 +1,11 @@
-import java.util.Hashtable;
+import java.util.ArrayList;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class BattleShip {
-
-    public static void PlayGame(Scanner Input) {
+    public static int[][] CreateAreaGame() {
+        Scanner Input = new Scanner(System.in);
         System.out.println("Hello, before we start, let's define the characteristics of the game");
         System.out.println("Select a numbers beetwen 5 - 10 for the play area");
         int AreaSpace = Input.nextInt();
@@ -11,8 +13,15 @@ public class BattleShip {
             System.out.println("you\'ve a mistake , try over put a valid input");
             AreaSpace = Input.nextInt();
         }
-        final int[][] Player1AreaSpace = new int[AreaSpace][AreaSpace];
-        final int[][] Player2AreaSpace = new int[AreaSpace][AreaSpace];
+        return new int[AreaSpace][AreaSpace];
+
+    }
+
+    public static void AddShip(int[][] Player, Scanner Input) {
+    }
+
+    public static void PlayGame(Scanner Input) {
+        int[][] Player1AreaSpace, Player2AreaSpace = CreateAreaGame();
 
     }
 
@@ -25,7 +34,7 @@ public class BattleShip {
         System.out.println(PresentationGameMensage);
         Scanner Input = new Scanner(System.in);
         String IntroduccionUserInput = Input.nextLine();
-        while (!IntroduccionUserInput.equals("play") ||
+        while (!IntroduccionUserInput.equals("play") &&
                 !IntroduccionUserInput.equals("help")) {
             System.out.println("you\'ve a mistake , try over put a valid input");
             IntroduccionUserInput = Input.nextLine();
