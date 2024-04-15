ec99872d1b8232456f8d23b575a511153df4b16e
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-04-15 11:07:10 -0500

    [feat➕] add feature to PlayGame Function to create game area

--- BattleShip.java
+++ BattleShip.java
@@ -2,7 +2,17 @@ import java.util.Hashtable;
 import java.util.Scanner;
 
 public class BattleShip {
-    public static void PlayGame() {
+
+    public static void PlayGame(Scanner Input) {
+        System.out.println("Hello, before we start, let's define the characteristics of the game");
+        System.out.println("Select a numbers beetwen 5 - 10 for the play area");
+        int AreaSpace = Input.nextInt();
+        while (AreaSpace < 5 && AreaSpace > 10) {
+            System.out.println("you\'ve a mistake , try over put a valid input");
+            AreaSpace = Input.nextInt();
+        }
+        final int[][] Player1AreaSpace = new int[AreaSpace][AreaSpace];
+        final int[][] Player2AreaSpace = new int[AreaSpace][AreaSpace];
 
     }
 
@@ -21,7 +31,7 @@ public class BattleShip {
             IntroduccionUserInput = Input.nextLine();
         }
         if (IntroduccionUserInput.equals("play"))
-            PlayGame();
+            PlayGame(Input);
         else {
 
             HelpUser();
